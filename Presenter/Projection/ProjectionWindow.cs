936e54c7c601d7c2d8808e45ebc16ac170d8e01a
ArgusMagnus <argusmagnus@outlook.com>
2017-04-21 10:57:38 +0200

    Fixed problems with slide animation

--- Presenter/Projection/ProjectionWindow.cs
+++ Presenter/Projection/ProjectionWindow.cs
@@ -27,6 +27,7 @@ using PraiseBase.Presenter.Properties;
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace PraiseBase.Presenter.Projection
 {
@@ -213,10 +214,9 @@ namespace PraiseBase.Presenter.Projection
 
 	public sealed class ProjectionWindow : Xwt.Window
     {
-		const int AnimationFps = 20;
+		const int AnimationFps = 60;
 		const int AnimationTpf = 1000 / AnimationFps;
 		const string WebViewStopUrl = "http://127.0.0.1/";
-
 
 		Image CurrentPreviewImage;
         Image CurrentPreviewText;
@@ -226,6 +226,10 @@ namespace PraiseBase.Presenter.Projection
 		readonly Xwt.ImageView _textImageBack = new Xwt.ImageView();
 		readonly Xwt.ImageView _textImage = new Xwt.ImageView() { Opacity = 0 };
 		readonly Xwt.Canvas _blackoutImage = new Xwt.Canvas() { Opacity = 0, BackgroundColor = Xwt.Drawing.Colors.Black };
+		AnimationCancellation _setProjectionCancellation = new AnimationCancellation();
+		AnimationCancellation _hideProjectionCancellation = new AnimationCancellation();
+		AnimationCancellation _setTextCancellation = new AnimationCancellation();
+		AnimationCancellation _blackoutCancellation = new AnimationCancellation();
 
 		static ProjectionWindow()
 		{
@@ -385,15 +389,18 @@ namespace PraiseBase.Presenter.Projection
 			{
 				_projectionImage.Opacity = 0f;
 				_projectionImage.Image = img.ToXwtImage();
-				int i = 0;
-				int frames = AnimationFps * fadeTime / 1000;
+				_setProjectionCancellation.RequestCancel();
+				_setProjectionCancellation = new AnimationCancellation();
+				var cancel = _setProjectionCancellation;
+				var watch = Stopwatch.StartNew();
 				Xwt.Application.TimeoutInvoke(AnimationTpf, () =>
 				{
-					i++;
-					_projectionImage.Opacity = (float)i / frames;
-					if (i >= frames)
+					if (cancel.CancelRequested)
+						return false;
+					_projectionImage.Opacity = Math.Min(1f, (float)watch.ElapsedMilliseconds / fadeTime);
+					if (_projectionImage.Opacity >= 1f)
 						_projectionImageBack.Image = _projectionImage.Image;
-					return i < frames;
+					return _projectionImage.Opacity < 1f;
 				});
 			}
 			else
@@ -408,15 +415,18 @@ namespace PraiseBase.Presenter.Projection
 			_projectionImageBack.Image = null;
 			if (fadeTime > 0)
 			{
-				int i = 0;
-				int frames = AnimationFps * fadeTime / 1000;
+				_hideProjectionCancellation.RequestCancel();
+				_hideProjectionCancellation = new AnimationCancellation();
+				var cancel = _hideProjectionCancellation;
+				var watch = Stopwatch.StartNew();
 				Xwt.Application.TimeoutInvoke(AnimationTpf, () =>
 				{
-					i++;
-					_projectionImage.Opacity = 1f - (float)i / frames;
-					if (i >= frames)
+					if (cancel.CancelRequested)
+						return false;
+					_projectionImage.Opacity = 1f - Math.Min(1f, (float)watch.ElapsedMilliseconds / fadeTime);
+					if (_projectionImage.Opacity <= 0f)
 						_projectionImage.Image = null;
-					return i < frames;
+					return _projectionImage.Opacity > 0f;
 				});
 			}
 			else
@@ -428,29 +438,29 @@ namespace PraiseBase.Presenter.Projection
 
 		void SetProjectionText(Bitmap img, int fadeTime)
 		{
-			//if (webBrowser.Source != null)
-			//{
-			//	HideWebsite();
-			//}
+			HideWebsite();
 
 			if (fadeTime > 0)
 			{
 				_textImage.Opacity = 0f;
 				_textImage.Image = img.ToXwtImage();
 
-				int i = 0;
-				int frames = AnimationFps * fadeTime / 1000;
+				_setTextCancellation.RequestCancel();
+				_setTextCancellation = new AnimationCancellation();
+				var cancel = _setTextCancellation;
+				var watch = Stopwatch.StartNew();
 				Xwt.Application.TimeoutInvoke(AnimationTpf, () =>
 				{
-					i++;
-					_textImage.Opacity = (float)i / frames;
+					if (cancel.CancelRequested)
+						return false;
+					_textImage.Opacity = Math.Min(1f, (float)watch.ElapsedMilliseconds / fadeTime);
 					_textImageBack.Opacity = 1f - _textImage.Opacity;
-					if (i >= frames)
+					if (_textImage.Opacity >= 1f)
 					{
 						_textImageBack.Image = _textImage.Image;
 						_textImageBack.Opacity = 1f;
 					}
-					return i < frames;
+					return _textImage.Opacity < 1f;
 				});
 			}
 			else
@@ -470,24 +480,29 @@ namespace PraiseBase.Presenter.Projection
 		{
 			if (animationTime > 0)
 			{
-				int i = 0;
-				int frames = AnimationFps * animationTime / 1000;
+				_blackoutCancellation.RequestCancel();
+				_blackoutCancellation = new AnimationCancellation();
+				var cancel = _blackoutCancellation;
 				if (enable)
 				{
+					var watch = Stopwatch.StartNew();
 					Xwt.Application.TimeoutInvoke(AnimationTpf, () =>
 					{
-						i++;
-						_blackoutImage.Opacity = (float)i / frames;
-						return i < frames;
+						if (cancel.CancelRequested)
+							return false;
+						_blackoutImage.Opacity = Math.Min(1f, (float)watch.ElapsedMilliseconds / animationTime);
+						return _blackoutImage.Opacity < 1f;
 					});
 				}
 				else
 				{
+					var watch = Stopwatch.StartNew();
 					Xwt.Application.TimeoutInvoke(AnimationTpf, () =>
 					{
-						i++;
-						_blackoutImage.Opacity = 1f - (float)i / frames;
-						return i < frames;
+						if (cancel.CancelRequested)
+							return false;
+						_blackoutImage.Opacity = 1f - Math.Min(1f, (float)watch.ElapsedMilliseconds / animationTime);
+						return _blackoutImage.Opacity > 0;
 					});
 				}
 			}
@@ -496,231 +511,11 @@ namespace PraiseBase.Presenter.Projection
 				_blackoutImage.Opacity = enable ? 1f : 0f;
 			}
 		}
+
+		class AnimationCancellation
+		{
+			public bool CancelRequested { get; private set; } = false;
+			public void RequestCancel() => CancelRequested = true;
+		}
 	}
 }
-
-///*
-// *   PraiseBase Presenter
-// *   The open source lyrics and image projection software for churches
-// *
-// *   http://praisebase.org
-// *
-// *   This program is free software; you can redistribute it and/or
-// *   modify it under the terms of the GNU General Public License
-// *   as published by the Free Software Foundation; either version 2
-// *   of the License, or (at your option) any later version.
-// *
-// *   This program is distributed in the hope that it will be useful,
-// *   but WITHOUT ANY WARRANTY; without even the implied warranty of
-// *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// *   GNU General Public License for more details.
-// *
-// *   You should have received a copy of the GNU General Public License
-// *   along with this program; if not, write to the Free Software
-// *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-// *
-// */
-
-//using System;
-//using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Interop;
-//using System.Windows.Media;
-//using System.Windows.Media.Animation;
-//using System.Windows.Media.Imaging;
-
-//namespace PraiseBase.Presenter.Projection
-//{
-//	/// <summary>
-//	/// Interaction logic for UserControl1.xaml
-//	/// </summary>
-//	///
-//	public partial class ProjectionControl : UserControl
-//	{
-//		#region Delegates
-
-//		public delegate void AnimationFinish(object sender, EventArgs e);
-
-//		#endregion Delegates
-
-//		public ProjectionControl()
-//		{
-//			InitializeComponent();
-//		}
-
-//		public event AnimationFinish AnimationFinished;
-
-//		private void ProjectionControlLoaded(object sender, RoutedEventArgs e)
-//		{
-//			webBrowser.LoadCompleted += WebBrowser_LoadCompleted;
-//		}
-
-//		/// <summary>
-//		/// Load a bitmap and convert it to a WPF image source
-//		/// </summary>
-//		/// <param name="source"></param>
-//		/// <returns></returns>
-//		protected static BitmapSource LoadBitmap(System.Drawing.Bitmap source)
-//		{
-//			return Imaging.CreateBitmapSourceFromHBitmap(source.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
-//		}
-
-//		/// <summary>
-//		/// Set a new image
-//		/// </summary>
-//		/// <param name="img">Image that will be shown</param>
-//		/// <param name="fadeTime">Animation time in miliseconds</param>
-//		public void SetProjectionImage(System.Drawing.Bitmap img, int fadeTime)
-//		{
-//			if (fadeTime > 0)
-//			{
-//				projectionImage.Opacity = 0f;
-//				projectionImage.Source = LoadBitmap(img);
-
-//				var imageAnimation = (Storyboard)FindResource("imageAnimation");
-//				imageAnimation.SpeedRatio = 1000f / fadeTime;
-//				imageAnimation.Begin(this);
-//			}
-//			else
-//			{
-//				projectionImage.Source = LoadBitmap(img);
-//				projectionImageBack.Source = projectionImage.Source;
-//			}
-//		}
-
-//		/// <summary>
-//		/// Fired when the animation is finished
-//		/// </summary>
-//		/// <param name="sender"></param>
-//		/// <param name="e"></param>
-//		private void ImageAnimationCompleted(object sender, EventArgs e)
-//		{
-//			projectionImageBack.Source = projectionImage.Source;
-//			AnimationFinished?.Invoke(this, new EventArgs());
-//		}
-
-//		public void HideProjectionImage(int fadeTime)
-//		{
-//			projectionImageBack.Source = null;
-//			if (fadeTime > 0)
-//			{
-//				var imageAnimation = (Storyboard)FindResource("imageHideAnimation");
-//				imageAnimation.SpeedRatio = 1000f / fadeTime;
-//				imageAnimation.Begin(this);
-//			}
-//			else
-//			{
-//				projectionImage.Opacity = 0f;
-//				projectionImage.Source = null;
-//			}
-//		}
-
-//		/// <summary>
-//		/// Fired when the animation is finished
-//		/// </summary>
-//		/// <param name="sender"></param>
-//		/// <param name="e"></param>
-//		private void ImageHideAnimationCompleted(object sender, EventArgs e)
-//		{
-//			projectionImage.Source = null;
-//			AnimationFinished?.Invoke(this, new EventArgs());
-//		}
-
-//		public void SetProjectionText(System.Drawing.Bitmap img, int fadeTime)
-//		{
-//			if (webBrowser.Source != null)
-//			{
-//				HideWebsite();
-//			}
-
-//			if (fadeTime > 0)
-//			{
-//				textImage.Opacity = 0f;
-//				textImage.Source = LoadBitmap(img);
-
-//				var imageAnimation2 = (Storyboard)FindResource("textAnimation2");
-//				imageAnimation2.SpeedRatio = 1000f / fadeTime;
-//				imageAnimation2.Begin(this);
-
-//				var imageAnimation = (Storyboard)FindResource("textAnimation");
-//				imageAnimation.SpeedRatio = 1000f / fadeTime;
-//				imageAnimation.Begin(this);
-//			}
-//			else
-//			{
-//				textImage.Source = LoadBitmap(img);
-//				textImageBack.Source = textImage.Source;
-//				textImageBack.Opacity = 1f;
-//			}
-//		}
-
-//		internal void Dispose()
-//		{
-//			webBrowser.Dispose();
-//		}
-
-//		private void TextAnimationCompleted(object sender, EventArgs e)
-//		{
-//			textImageBack.Source = textImage.Source;
-//			textImageBack.Opacity = 1f;
-//			AnimationFinished?.Invoke(this, new EventArgs());
-//		}
-
-//		public void ShowWebsite(Uri uri)
-//		{
-//			webBrowser.Source = uri;
-//		}
-
-//		public void HideWebsite()
-//		{
-//			webBrowser.Visibility = Visibility.Collapsed;
-//			webBrowser.Source = null;
-//		}
-
-//		private void WebBrowser_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
-//		{
-//			if (e.Uri != null)
-//			{
-//				webBrowser.Visibility = Visibility.Visible;
-//			}
-//		}
-
-//		/// <summary>
-//		/// Toggle blackout mode
-//		/// </summary>
-//		/// <param name="val">True if blackout should be enabled, else false</param>
-//		/// <param name="animationTime">Animation duration in miliseconds</param>
-//		public void BlackOut(bool val, int animationTime)
-//		{
-//			Storyboard blackoutAnimation = (Storyboard)FindResource(val ? "blackoutAnimationOn" : "blackoutAnimationOff");
-//			blackoutAnimation.SpeedRatio = (animationTime == 0 ? 100 : 1000f / animationTime);
-//			blackoutAnimation.Begin(this);
-//		}
-
-//		//public void SetText(List<TextBlock> textBlocks)
-//		//{
-//		//    TextBlock bl = textBlocks[0];
-//		//    String str = string.Empty;
-//		//    for (int i = 0; i < bl.Lines.Count; i++)
-//		//    {
-//		//        str += bl.Lines[i].Text + "\n";
-//		//        ;
-//		//    }
-//		//    //textBlock1.Text = str;
-//		//    //textBlock1.Width = this.Width - (2 * textBlock1.Margin.Left);
-//		//    //textBlock1.Height = this.Height - (2 * textBlock1.Margin.Top);
-//		//}
-
-//		/*
-//        TextContent.Text = "aasdfasdfasdf\naasdfasdfasdf\naasdfasdfasdf\naasdfasdfasdf\naasdfasdfasdf\n";
-//        TextContent.Width = this.Width - (2 * TextContent.Margin.Left);
-//        TextContent.Height = this.Height - (2 * TextContent.Margin.Top);
-//        */
-//		/*
-//                    textBlock1.Text = "Animation: "+fadeTime+" ms";
-//                    textBlock1.Visibility = System.Windows.Visibility.Visible;
-//                    textBlock1.Width = this.Width - (2 * textBlock1.Margin.Left);
-//                    textBlock1.Height = this.Height - (2 * textBlock1.Margin.Top);
-//                    */
-//	}
-//}
